c29e4c30372701204acc53f387076c7bff892b5d
thoaiky <thoaiky1992@gmail.com>
2022-02-05 15:58:20 +0700

    config router

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
 
   <h1 v-if="app">{{ `${app.id}---${app.app_name}` }}</h1>
   <h1 v-else>loading...</h1>
+  <button @click="directRoute('/')">Route Home</button>
+  <button @click="directRoute('/about')">Route About</button>
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
@@ -12,16 +15,22 @@ import AppEntity from '@/entities/app.entity';
 import HelloWorld from '@/components/HelloWorld.vue';
 import { useAppService } from '@/services/app.service';
 import { Socket } from 'socket.io-client';
+import { useRouter } from 'vue-router';
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
   },
   setup() {
-    const socket: Socket | undefined = inject('socket') as Socket;
+    // const socket: Socket | undefined = inject('socket') as Socket;
     const a = 'Hello world !!';
+    const router = useRouter();
     let data = reactive<{ app: AppEntity | undefined }>({ app: undefined });
     const service = useAppService();
+
+    const directRoute = (path: string) => {
+      router.push(path);
+    };
 
     (() => {
       setTimeout(async () => {
@@ -29,13 +38,13 @@ export default defineComponent({
       }, 1000);
     })();
 
-    onMounted(() => {
-      socket.on('socket', (data: any) => {
-        console.log(data);
-      });
-    });
+    // onMounted(() => {
+    //   socket.on('socket', (data: any) => {
+    //     console.log(data);
+    //   });
+    // });
 
-    return { a, ...toRefs(data) };
+    return { a, ...toRefs(data), directRoute };
   }
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import App from '@/App.vue';
 import '@/index.css';
-import { PluginSocketIo } from '@/plugins/socket.io';
+// import { PluginSocketIo } from '@/plugins/socket.io';
 import { store } from './vuex';
+import router from './router';
 
 const app = createApp(App);
-app.use(PluginSocketIo, '/');
+// app.use(PluginSocketIo, '/');
 app.use(store);
+app.use(router);
 
 app.mount('#app');
